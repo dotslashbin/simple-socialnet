8d4b942121fa90abb986c650efa45c1141d00d25
JoshuaF <joshuarpf@gmail.com>
2018-12-13 23:36:08 +0800

    - installed dependencies - initial use of users.js , routed file

--- server.js
+++ server.js
@@ -3,12 +3,23 @@
  */
 const express 	= require('express')
 const app 		= express() 
+const mongoose 	= require('mongoose')
+
+// DB Config
+const db 		= require('./config/keys').mongoURI
+
+// Required Routes
+const users 	= require('./routes/api/users.js')
 
 /**
  * Initial Variables
  */
  const port 	= process.env.PORT || 5000
 
-app.get('/', (req, res) => res.send('Hello from server .. additional testing'))
+// Default
+app.get('/', (req, res) => res.send('Hello from e .. additional testing'))
+
+// Routes
+app.use('/api/users', users)
 
 app.listen(port, () => console.log('SimpleSocial running on port: ' + port))	
