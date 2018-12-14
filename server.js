fe0899830600c3d9514bc6c151a78acdf15df30f
JoshuaF <joshuarpf@gmail.com>
2018-12-14 21:45:10 +0800

    Implemented routes

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 /**
  * Includes
  */
-const express 	= require('express')
-const app 		= express() 
-const mongoose 	= require('mongoose')
+const express 		= require('express')
+const app 			= express() 
+const mongoose 		= require('mongoose')
+const bodyParser 	= require('body-parser')
 
 // DB Config
 const db 		= require('./config/keys').mongoURI
 
 // Required Routes
 const users 	= require('./routes/api/users.js')
+const profile 	= require('./routes/api/profile.js')
+const posts 	= require('./routes/api/posts.js')
+
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
 
 // Connect to mongodb
 mongoose.connect(db).then(() => console.log('Mongodb database connected')).catch((err) => console.log(err))
@@ -23,6 +29,10 @@ mongoose.connect(db).then(() => console.log('Mongodb database connected')).catch
 app.get('/', (req, res) => res.send('Hello from e .. additional testing'))
 
 // Routes
+// app.use('/api/users', users)
+// app.use('/api/profile', profile)
+// app.use('/api/posts', posts)
+
 app.use('/api/users', users)
 
 
