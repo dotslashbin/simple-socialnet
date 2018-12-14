999950eb0aa46e98cbe5876954f4c6c8c38f6fa4
JoshuaF <joshuarpf@gmail.com>
2018-12-14 21:45:34 +0800

    Implemented saving of new user account if it does not exists

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,49 @@
-const express 	= require('express')
-const router 	= express.Router()
+const express 	= require('express');
+const router 	= express.Router();
+const gravatar 	= require('gravatar')
+const bcrypt 	= require('bcrypt')
 
-router.get('/', (req, res) => res.send('from user file'))
+/**
+ * Models
+ */
+const User 		= require('../../models/User')
+
+router.get('/test', (req, res) => res.send('from user file'))
+
+/**
+ * Registration
+ */
+router.post('/register', (req, res) => {
+	User.findOne({ email: req.body.email })
+	.then((user) => {
+		if(user) {
+			return res.status(400).json({ email: 'Email already exists'})
+		} else {
+			const avatar = new gravatar.url(req.body.email, {
+				s: '200', 
+				r: 'pg', 
+				d: 'mm'
+			})
+
+			const newUser = new User({
+				name: req.body.name, 
+				email: req.body.email,
+				password: req.body.password,
+				avatar: 'http://test.com'
+			});
+
+			bcrypt.genSalt(10, (err, salt) => {
+				bcrypt.hash(newUser.password, salt, (err, hash) => {
+					newUser.password = hash
+					newUser.save().then(user => res.json(user)).catch(err => console.log(err))
+				})
+			})
+
+			// todo: delete this
+			// newUser.save()
+			
+		}
+	})
+})
 
 module.exports = router
