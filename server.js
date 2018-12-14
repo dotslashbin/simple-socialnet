229138e749033b79c36a3b1b58de08da913544d3
JoshuaF <joshuarpf@gmail.com>
2018-12-14 20:23:37 +0800

    Added mongo db connection

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ const db 		= require('./config/keys').mongoURI
 // Required Routes
 const users 	= require('./routes/api/users.js')
 
+// Connect to mongodb
+mongoose.connect(db).then(() => console.log('Mongodb database connected')).catch((err) => console.log(err))
+
 /**
  * Initial Variables
  */
@@ -21,5 +24,6 @@ app.get('/', (req, res) => res.send('Hello from e .. additional testing'))
 
 // Routes
 app.use('/api/users', users)
+
 
 app.listen(port, () => console.log('SimpleSocial running on port: ' + port))	
