282a5b0d05609ea652ef924bd238aa46ccb3731e
JoshuaF <joshuarpf@gmail.com>
2018-12-15 17:15:02 +0800

    Cleaned up

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,51 +1,54 @@
-const express 	= require('express');
-const router 	= express.Router();
-const gravatar 	= require('gravatar')
-const bcrypt 	= require('bcrypt')
+const express 		= require('express');
+const router 		= express.Router();
+const gravatar 		= require('gravatar')
+const bcrypt 		= require('bcrypt')
 
 /**
  * Models
  */
 const User 		= require('../../models/User')
 
-router.get('/test', (req, res) => res.send('from user file'))
+router.get('/test', (requests, response) => response.send('from user file'))
 
 /**
  * Registration
  */
-router.post('/register', (req, res) => {
-	User.findOne({ email: req.body.email })
+router.post('/register', (requests, response) => {
+	User.findOne({ email: requests.body.email })
 	.then((user) => {
 		if(user) {
-			return res.status(400).json({ email: 'Email already exists'})
+			return response.status(400).json({ email: 'Email already exists'})
 		} else {
-			const avatar = gravatar.url(req.body.email, {
+			const avatar = gravatar.url(requests.body.email, {
 		        s: '200', // Size
 		        r: 'pg', // Rating
 		        d: 'mm' // Default
 	      	});
 
 			const newUser = new User({
-				name: req.body.name, 
-				email: req.body.email,
-				password: req.body.password,
-				avatar
+				name: requests.body.name, 
+				email: requests.body.email,
+				avatar, 
+				password: requests.body.password
 			});
-
-			console.log(`New User: ${newUser}`)
 
 			bcrypt.genSalt(10, (err, salt) => {
 				bcrypt.hash(newUser.password, salt, (err, hash) => {
 					newUser.password = hash
-					newUser.save().then(user => res.json(user)).catch(err => console.log(err))
+					newUser.save()
+					.then(user => response.json(user))
+					.catch(err => console.log(err))
 				})
 			})
-
-			// todo: delete this
-			// newUser.save()
 		}
-			
 	})
 })
+
+/**
+ * Login
+ */
+ router.post('/login', (requests, response) => {
+
+ })
 
 module.exports = router
