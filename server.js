617f9ca9f492d00f6892940a93a7d5c9df580001
JoshuaF <joshuarpf@gmail.com>
2018-12-17 11:12:59 +0800

    - Added comments - Added implmenetation on fetching authenticated user data

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-/**
- * Includes
- */
+/////////////
+// Modules //
+/////////////
 const express 		= require('express')
 const app 			= express() 
 const mongoose 		= require('mongoose')
 const bodyParser 	= require('body-parser')
+const passport 		= require('passport')
 
-// DB Config
+///////////////
+// DB Config //
+///////////////
 const db 		= require('./config/keys').mongoURI
 
-// Required Routes
+//////////////////////
+// Required Routes  //
+//////////////////////
 const users 	= require('./routes/api/users.js')
 const profile 	= require('./routes/api/profile.js')
 const posts 	= require('./routes/api/posts.js')
@@ -17,23 +22,45 @@ const posts 	= require('./routes/api/posts.js')
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
 
-// Connect to mongodb
+/////////////////////////
+// Connect to mongodb  //
+/////////////////////////
 mongoose.connect(db).then(() => console.log('Mongodb database connected')).catch((err) => console.log(err))
 
-/**
- * Initial Variables
- */
- const port 	= process.env.PORT || 5000
+//////////////////////////
+// Passport middleware  //
+//////////////////////////
+app.use(passport.initialize())
 
-// Default
+//////////////////////
+// Passport config  //
+//////////////////////
+/**
+ * This basically passes the passport variable ( which is the module ) INTO the config
+ * file as a parameter
+ */
+require('./config/passport')(passport)
+
+////////////////////////
+// Initial variables  //
+////////////////////////
+const port 	= process.env.PORT || 5000
+
+//////////////
+// Default  //
+//////////////
 app.get('/', (requests, response) => response.send('Hello from e .. additional testing'))
 
-// Routes
+/////////////
+// Routes  //
+/////////////
 app.use('/api/users', users)
 // app.use('/api/profile', profile)
 // app.use('/api/posts', posts)
 
 app.use('/api/users', users)
 
-
+///////////////////////
+// Run's the server  //
+///////////////////////
 app.listen(port, () => console.log('SimpleSocial running on port: ' + port))	
