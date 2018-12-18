9e19058234ffb04b4e298055e0214ce048400ac9
JoshuaF <joshuarpf@gmail.com>
2018-12-18 13:18:44 +0800

    initial validation

---
+++ validation/profile.js
@@ -0,0 +1,38 @@
+/////////////
+// Modules //
+/////////////
+const Validator 	= require('validator')
+const isEmpty 		= require('./is-empty')
+
+////////////////////
+// Included files //
+////////////////////
+// import isEmpty from "./is-empty"
+
+module.exports = function validateProfileInput(data) {
+	let errors 	= {}
+
+	data.email 		= !isEmpty(data.email)? data.email: ''
+	data.password 	= !isEmpty(data.password)? data.password: ''
+
+	if(!Validator.isEmail(data.email)) {
+		errors.email = 'Email is invalid'
+	}
+
+	if(Validator.isEmpty(data.email)) {
+		errors.email = 'Email is required'
+	}
+
+	if(Validator.isEmpty(data.password)) {
+		errors.password = 'Password field is required'
+	}
+
+	if(!Validator.isLength(data.password, { min:6, max:30 } )) {
+		errors.password = 'Password length must be at least 6 characters'
+	}
+
+	return {
+		errors, 
+		isValid: isEmpty(errors)
+	}
+} 
