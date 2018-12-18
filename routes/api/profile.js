4d1842290f6648a33298f655c09e5380f2eead52
JoshuaF <joshuarpf@gmail.com>
2018-12-18 13:18:31 +0800

    Implemented saving of profile ( update and new )

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -33,5 +33,60 @@ router.get('/', passport.authenticate('jwt', { session:false }), (request, respo
 	})
 })
 
+/**
+ * create or edit user profile 
+ * @param  {[type]} '/create-profile'            [description]
+ * @param  {[type]} passport.authenticate('jwt', {            session:      false }) [description]
+ * @param  {[type]} (request,                    response      [description]
+ * @return {[type]}                              [description]
+ */
+router.post('/save', passport.authenticate('jwt', { session: false }), (request, response) => {
+
+	// Get fields
+    const profileFields 										= {};
+    profileFields.user 											= request.user.id;
+    if (request.body.handle) profileFields.handle 				= request.body.handle;
+    if (request.body.company) profileFields.company 			= request.body.company;
+    if (request.body.website) profileFields.website 			= request.body.website;
+    if (request.body.location) profileFields.location 			= request.body.location;
+    if (request.body.bio) profileFields.bio 					= request.body.bio;
+    if (request.body.status) profileFields.status 				= request.body.status;
+    if (request.body.githubUsername)
+      profileFields.githubUsername 								= request.body.githubUsername;
+    // Skills - Spilt into array
+    if (typeof request.body.skills !== 'undefined') {
+      profileFields.skills 										= request.body.skills.split(',');
+    }
+
+    // Social
+    profileFields.social 										= {};
+    if (request.body.youtube) profileFields.social.youtube 		= request.body.youtube;
+    if (request.body.twitter) profileFields.social.twitter 		= request.body.twitter;
+    if (request.body.facebook) profileFields.social.facebook 	= request.body.facebook;
+    if (request.body.linkedin) profileFields.social.linkedin 	= request.body.linkedin;
+    if (request.body.instagram) profileFields.social.instagram 	= request.body.instagram;
+
+    Profile.findOne({ user: request.user.id }).then(profile => {
+	    
+    	if(Profile) {
+			// This means an update
+    		Profile.findOneAndUpdate({ user:request.user.id }, { $set: profileFields }, { new: true }).then(profile => respond.json(profile))
+    	} else {
+    		// This means creating a new profile
+    		
+    		// Check to see if the handle exists to avoid duplication
+    		Profile.findOne({ handle: profileFields.handle }).then(Profile => {
+    			if(profile) {
+    				errors.handle = 'Handle already existst'
+    				respond.status(400).json(errors)
+    			}
+
+    			// Save Profile 
+    			new Profile(profileFileds).save().then(savedProfile => respond.json(savedProfile))
+    		})
+    	}
+    })
+})
+
 
 module.exports 		= router
