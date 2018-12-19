bc6135d300f7e1602aae9cc4546625fe3c2a2c18
JoshuaF <joshuarpf@gmail.com>
2018-12-19 14:38:23 +0800

    Converted to ES6 style

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -14,27 +14,50 @@ class Register extends Component {
 		/////////////////////////////////////////////////////////////////
 		// Methods binding: Necessary to identify the 'this' from call //
 		/////////////////////////////////////////////////////////////////
-		this.onChange = this.onChange.bind(this)
-		this.onSubmit = this.onSubmit.bind(this)
+		// this.onChange = this.onChange.bind(this) // Method 1
+		// this.onSubmit = this.onSubmit.bind(this) // Method 1
 	}
 
 	/**
+	 * Method 1: where you need to bind the method on the constructor
 	 * [onChange description]
 	 * @param  {[type]} event [description]
 	 * @return {[type]}       [description]
 	 */
-	onChange(event) {
+	// onChange(event) {
 
-		// This is basically the same as setting value for each field in the state
-		this.setState({ [event.target.name]: event.target.value });
-	}
+	// 	// This is basically the same as setting value for each field in the state
+	// 	this.setState({ [event.target.name]: event.target.value });
+	// }
+	
+	/**
+	 * The same onChange method but there is no need to bind since it is in ES6
+	 * @param  {[type]} event [description]
+	 * @return {[type]}       [description]
+	 */
+	onChange = (event) => { this.setState({ [event.target.name]: event.target.value }); }
 
 	/**
 	 * Submitting the registration
 	 * @param  {[type]} event [description]
 	 * @return {[type]}       [description]
 	 */
-	onSubmit(event) {
+	// onSubmit(event) {
+	// 	event.preventDefault()
+
+	// 	const newUser =  {
+	// 		name: this.state.name,
+	// 		email: this.state.email, 
+	// 		password: this.state.password, 
+	// 		password2: this.state.password2
+	// 	}
+
+	// 	alert(newUser)
+
+	// 	console.log(newUser)
+	// }
+	// 
+	onSubmit = (event) => {
 		event.preventDefault()
 
 		const newUser =  {
@@ -44,9 +67,8 @@ class Register extends Component {
 			password2: this.state.password2
 		}
 
-		alert(newUser)
-
 		console.log(newUser)
+	
 	}
 
 	render() {
