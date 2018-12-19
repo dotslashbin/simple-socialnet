9d980b17954df3211b3bbd187306570aa359a853
JoshuaF <joshuarpf@gmail.com>
2018-12-19 12:13:01 +0800

    Changed link to using Link

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,4 +1,8 @@
+///////////////////////////////
+// Imported react components //
+///////////////////////////////
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Landing extends Component {
 	render() {
@@ -12,8 +16,8 @@ class Landing extends Component {
 								</h1>
 								<p className="lead"> Create a developer profile/portfolio, share posts and get help from other developers</p>
 								<hr />
-								<a href="register.html" className="btn btn-lg btn-info mr-2">Sign Up</a>
-								<a href="login.html" className="btn btn-lg btn-light">Login</a>
+								<Link to="Register" className="btn btn-lg btn-info mr-2">Sign Up</Link>
+								<Link to="Login" className="btn btn-lg btn-light">Login</Link>
 							</div>
 						</div>
 					</div>
