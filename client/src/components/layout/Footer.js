db3fb8b4c74ad0c35f659f6d813f5782718d5500
JoshuaF <joshuarpf@gmail.com>
2018-12-19 11:37:25 +0800

    Added initial layout components

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+var Footer = () => {
+	return (
+		<footer className="bg-dark text-white mt-5 p-4 text-center">
+			Copyright &copy; {new Date().getFullYear()} Joshua's App
+		</footer>
+	)
+}
+
+export default Footer
